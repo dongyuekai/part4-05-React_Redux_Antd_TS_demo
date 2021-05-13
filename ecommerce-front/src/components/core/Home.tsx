569582282801b766261078ca6467701aaa27f4f9
“dongyuekai” <457545648@qq.com>
2021-05-13 14:55:40 +0800

    创建导航菜单Menu

--- ecommerce-front/src/components/core/Home.tsx
+++ ecommerce-front/src/components/core/Home.tsx
@@ -4,7 +4,6 @@ import { useSelector } from 'react-redux'
 
 const Home = () => {
   const state = useSelector(state => state)
-  console.log(state)
   return <Layout>Home:{JSON.stringify(state)}</Layout>
 }
 export default Home
--- ecommerce-front/src/components/core/Layout.tsx
+++ ecommerce-front/src/components/core/Layout.tsx
@@ -1,9 +1,15 @@
 import React, { FC } from 'react'
+import Navigation from './Navigation'
 
 interface Props {
   children: React.ReactNode
 }
 const Layout: FC<Props> = ({ children }) => {
-  return <div>Layout{children}</div>
+  return (
+    <div>
+      <Navigation />
+      {children}
+    </div>
+  )
 }
 export default Layout
